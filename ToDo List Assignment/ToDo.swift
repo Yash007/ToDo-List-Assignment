8ebcc3ffbe52594bae2d9cb64415850b4063d315
YASH SOMPURA <yash007@yashs-macbook-pro.local>
2018-01-03 17:27:28 -0500

    implement Serialization(encoding) for local storage

--- ToDo List Assignment/ToDo.swift
+++ ToDo List Assignment/ToDo.swift
@@ -8,13 +8,34 @@
 
 import Foundation
 
-class ToDo  {
+class ToDo: NSObject, NSCoding {
     var title:String
     var done:Bool
     
     public init(title:String)   {
         self.title = title
         self.done = false
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        if let title = aDecoder.decodeObject(forKey: "title") as? String    {
+            self.title = title
+        }
+        else    {
+            return nil
+        }
+        
+        if aDecoder.containsValue(forKey: "done")   {
+            self.done = aDecoder.decodeBool(forKey: "done")
+        }
+        else    {
+            return nil
+        }
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.title, forKey: "title")
+        aCoder.encode(self.done, forKey: "done")
     }
 }
 
