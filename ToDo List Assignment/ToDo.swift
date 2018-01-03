9765e3a8f569cfd3c810cb8228e19f6d9eea32f9
YASH SOMPURA <yash007@yashs-macbook-pro.local>
2018-01-03 16:36:49 -0500

    Application Created

---
+++ ToDo List Assignment/ToDo.swift
@@ -0,0 +1,31 @@
+//
+//  ToDo.swift
+//  ToDo List Assignment
+//
+//  Created by YASH SOMPURA on 2018-01-03.
+//  Copyright © 2018 YASH SOMPURA. All rights reserved.
+//
+
+import Foundation
+
+class ToDo  {
+    var title:String
+    var done:Bool
+    
+    public init(title:String)   {
+        self.title = title
+        self.done = false
+    }
+}
+
+extension ToDo  {
+    public class func getMockData() -> [ToDo]   {
+        return[
+            ToDo(title: "Milk"),
+            ToDo(title: "Egg"),
+            ToDo(title: "Chocolate"),
+            ToDo(title: "Bread"),
+            ToDo(title: "Apple")
+        ]
+    }
+}
